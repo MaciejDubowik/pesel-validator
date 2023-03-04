6accd363559eaed06727aeab29e76672ab9479ad
MaciejDubowik <maciejxpoland@gmail.com>
2023-03-04 22:18:17 +0100

    implement logic

--- pesel validator/ViewController.swift
+++ pesel validator/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var peselTextField: UITextField!
+    @IBAction func submitButtonPressed(_ sender: UIButton) {
+        print(checkPesel(peselTextField.text!))
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    var sum = 0
+    var checkDigit = 0
+    let weights = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3]
+    let year = ["0": "19", "1": "19", "2": "20", "3": "20", "4": "21", "5": "21", "6": "22", "7": "22", "8": "18", "9": "18"]
+    let monthSingleDigit = ["1": "01", "2": "02", "3": "03", "4": "04", "5": "05", "6": "06", "7": "07", "8": "08", "9": "09"]
+    let monthTwoDigit = ["0": "10", "1": "11", "2": "12"]
+    let results = [
+        (status: false, message: "Pesel musi składać się z 11 cyfr"),
+        (status: false, message: "Pesel nieprawidłowy"),
+        (status: true, message: "Pesel prawidłowy")
+    ]
+    
+    func checkPesel(_ pesel: String) -> (status: Bool, message: String) {
+        var peselArr = pesel.map { Int(String($0))! }
+        sum = 0
+        peselArr.enumerated().forEach { (index, element) in
+            if index == peselArr.count - 1 { return }
+            sum += element * weights[index]
+        }
+        checkDigit = 10 - (sum % 10)
+        if peselArr.count != 11 { return results[0] }
+        if checkDigit == peselArr[10] { return results[2] }
+        return results[1]
+    }
 
 }
 
